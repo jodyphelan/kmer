321b119bab6dc7dc821d228c15edcc2dd90c7516
Jody Phelan <jody.phelan@lshtm.ac.uk>
2017-10-30 17:43:41 +0000

    first commit

---
+++ find_kmer_matches.py
@@ -0,0 +1,51 @@
+import sys
+import subprocess
+
+def fa2dict(filename):
+        fa_dict = {}
+        seq_name = ""
+        for l in open(filename):
+                line = l.rstrip()
+                if line[0] == ">":
+                        seq_name = line[1:].split()[0]
+                        fa_dict[seq_name] = []
+                else:
+                        fa_dict[seq_name].append(line)
+        result = {}
+        for seq in fa_dict:
+                result[seq] = "".join(fa_dict[seq])
+        return result
+
+def revcom(x):
+	return "".join([{"A":"T","C":"G","G":"C","T":"A"}[x[i]] for i in range(len(x)-1,-1,-1)])
+
+
+
+if len(sys.argv)!=5:
+	print "find_kmer_matches.py <kmer.fa> <genome.fa> <outfile.txt> <threads>"; quit()
+kmerfile = sys.argv[1]
+genomefile = sys.argv[2]
+outfile = sys.argv[3]
+threads = sys.argv[4]
+fdict = fa2dict(kmerfile)
+FNULL = open("/dev/null","w")
+for klen in sorted(set([len(x) for x in fdict.values()])):
+	print "Counting kmers with length=%s" % (klen)
+	subprocess.call("dsk -nb-cores %s -file %s -kmer-size %s -out %s.k%s.h5 && dsk2ascii -file %s.k%s.h5 -out %s.k%s.txt" % (threads,genomefile,klen,genomefile,klen,genomefile,klen,genomefile,klen),shell=True,stderr=FNULL,stdout=FNULL)
+
+FNULL.close()
+
+O = open(outfile,"w")	
+for kname in fdict:
+	kseq = fdict[kname]
+	krc = revcom(kseq)
+	klen = len(kseq)
+	kfile = "%s.k%s.txt" % (genomefile,klen)
+	num = 0
+	for l in open(kfile):
+		arr = l.rstrip().split()
+		if arr[0]==kseq or arr[0]==krc:
+			num+=int(arr[1])
+	O.write("%s\t%s\t%s\n" % (kname,kseq,num))
+O.close()
+
